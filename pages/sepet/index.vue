0d39cd54c7c15cf8ddc4a43ebe8b250699266aed
ece-duman <eceduman943@gmail.com>
2025-01-20 13:27:40 +0300

    sepet\pages
    sepet index sayfası

---
+++ pages/sepet/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SepetComponent from '~/components/sepetComponent.vue';
+
+</script>
+
+<template>
+    <ComponentOne/>
+    <ComponentEight/>
+    <sepetComponent/>
+    <ComponentSix/>
+</template>
