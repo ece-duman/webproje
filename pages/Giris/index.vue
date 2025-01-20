5b2717b4e1d0f993e1c16b21bfff83fd6c504b91
ece-duman <eceduman943@gmail.com>
2025-01-20 13:27:11 +0300

    giriş/index
    giriş in index sayfası

---
+++ pages/Giris/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ComponentOne from '~/components/ComponentOne.vue';
+
+</script>
+
+<template>
+    <ComponentOne/>
+    <ComponentEight/>
+    <giris />
+    <ComponentSix/>
+</template>
